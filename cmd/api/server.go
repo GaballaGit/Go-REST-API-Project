ad1286fb2c53525a99bbed1e42fd772e1e4bf422
Mark Gaballa <mgaballa@csu.fullerton.edu>
2025-08-11 16:39:52 -0700

    Reorganizing api

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -2,179 +2,15 @@ package main
 
 import (
 	"crypto/tls"
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"strconv"
-	"strings"
-	"sync"
 	"time"
 
+	"restapi/internal/api/handlers"
 	mw "restapi/internal/api/middlewares"
+	"restapi/pkg/utils"
 )
-
-type Teacher struct {
-	ID        int    `json:"id"`
-	FirstName string `json:"firstname"`
-	LastName  string `json:"lastname"`
-	Class     string `json:"class"`
-	Subject   string `json:"subject"`
-}
-
-var (
-	teachers = make(map[int]Teacher)
-	mutex    = &sync.Mutex{}
-	nextId   = 1
-)
-
-func init() {
-	teachers[nextId] = Teacher{
-		ID:        nextId,
-		FirstName: "John",
-		LastName:  "Doe",
-		Class:     "9A",
-		Subject:   "Math",
-	}
-	nextId++
-	teachers[nextId] = Teacher{
-		ID:        nextId,
-		FirstName: "Luwo",
-		LastName:  "Ko",
-		Class:     "7B",
-		Subject:   "Physics",
-	}
-	nextId++
-}
-
-func getTeachersHandler(w http.ResponseWriter, r *http.Request) {
-	teacherList := make([]Teacher, 0, len(teachers))
-	for _, teacher := range teachers {
-		teacherList = append(teacherList, teacher)
-	}
-
-	path := strings.TrimPrefix(r.URL.Path, "/teachers/")
-	idStr := strings.TrimSuffix(path, "/")
-
-	if idStr == "" {
-		response := struct {
-			Status string    `json:"status"`
-			Count  int       `json:"count"`
-			Data   []Teacher `json:"data"`
-		}{
-			Status: "success",
-			Count:  len(teacherList),
-			Data:   teacherList,
-		}
-
-		w.Header().Set("content-type", "application/json")
-		json.NewEncoder(w).Encode(response)
-	}
-
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	teacher, exists := teachers[id]
-	if !exists {
-		http.Error(w, "error, teacher with id not found", http.StatusNotFound)
-		return
-	}
-	json.NewEncoder(w).Encode(teacher)
-}
-
-func addTeachersHandler(w http.ResponseWriter, r *http.Request) {
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	var newTeachers []Teacher
-	err := json.NewDecoder(r.Body).Decode(&newTeachers)
-	if err != nil {
-		http.Error(w, "error decoding json", http.StatusBadRequest)
-		return
-	}
-
-	addedTeachers := make([]Teacher, len(newTeachers))
-	for i, newTeacher := range newTeachers {
-		newTeacher.ID = nextId
-		teachers[nextId] = newTeacher
-		addedTeachers[i] = newTeacher
-		nextId++
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	response := struct {
-		Status string    `json:"status"`
-		Count  int       `json:"count"`
-		Data   []Teacher `json:"data"`
-	}{
-		Status: "success",
-		Count:  len(addedTeachers),
-		Data:   addedTeachers,
-	}
-
-	json.NewEncoder(w).Encode(response)
-}
-
-func handleRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("REQUEST:", r.Method)
-	err := r.ParseForm()
-	if err != nil {
-		http.Error(w, "error parsing form", http.StatusBadRequest)
-		return
-	}
-	w.Write([]byte("Hello root server!"))
-	/*fmt.Println("form:", r.Form)
-	process := make(map[string]interface{})
-	for key, value := range r.Form {
-		process[key] = value
-	}
-	fmt.Println("Processed map:", process)
-	*/
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "error reading body", http.StatusBadRequest)
-	}
-	fmt.Println("body:", string(body))
-	fmt.Println("context:", r.Context())
-	fmt.Println("content len:", r.ContentLength)
-	fmt.Println(r)
-}
-
-func handleTeachers(w http.ResponseWriter, r *http.Request) {
-	// teachers/{id}
-	// teachers/?key=value&querry=value2&sortby=email&sortorder=ASC
-	if r.Method == http.MethodGet {
-		getTeachersHandler(w, r)
-	}
-
-	if r.Method == http.MethodGet {
-		addTeachersHandler(w, r)
-	}
-
-	path := strings.TrimPrefix(r.URL.Path, "/teachers/")
-	id := strings.TrimSuffix(path, "/")
-
-	fmt.Println("The teacher id:", id)
-
-	querries := r.URL.Query()
-	for key, value := range querries {
-		fmt.Println(key, "\t|", value)
-	}
-	w.Write([]byte("Hello teachers route!"))
-}
-
-func handleStudents(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello students route!"))
-}
-
-func handleExecs(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Execs route!"))
-}
 
 func main() {
 
@@ -198,7 +34,7 @@ func main() {
 	}
 
 	//secureMux := mw.Cors(rl.MiddleWare(mw.ResponseTime(mw.Compression(mw.SecurityHeader(mw.Hpp(hpp)(mux))))))
-	secureMux := applyMiddlewares(
+	secureMux := utils.ApplyMiddlewares(
 		mux,
 		mw.Hpp(hpp),
 		mw.Compression,
@@ -214,10 +50,10 @@ func main() {
 		TLSConfig: tlsConfig,
 	}
 
-	mux.HandleFunc("/", handleRoot)
-	mux.HandleFunc("/teachers/", handleTeachers)
-	mux.HandleFunc("/students/", handleStudents)
-	mux.HandleFunc("/execs/", handleExecs)
+	mux.HandleFunc("/", handlers.RootHandler)
+	mux.HandleFunc("/teachers/", handlers.TeacherHandler)
+	mux.HandleFunc("/students/", handlers.StudentHandler)
+	mux.HandleFunc("/execs/", handlers.ExecHandler)
 
 	fmt.Println("server started at port:", PORT)
 	err := server.ListenAndServeTLS(cert, key)
@@ -225,14 +61,4 @@ func main() {
 		log.Fatalf("error starting server: %s", err)
 		return
 	}
-}
-
-// Make middlewares cleaner
-type MiddleWare func(http.Handler) http.Handler
-
-func applyMiddlewares(handler http.Handler, middlewares ...MiddleWare) http.Handler {
-	for _, middleware := range middlewares {
-		handler = middleware(handler)
-	}
-	return handler
 }
